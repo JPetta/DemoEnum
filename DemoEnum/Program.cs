cf7d1530f647b74247cc8552c058c59c80ce3767
JPetta <jovipetraratulangi@gmail.com>
2019-04-09 18:35:41 +0700

    first commit

---
+++ DemoEnum/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace DemoEnum
+{
+    public enum ShippingMethod
+    {
+        RegularAirMail = 1,
+        RegistersteredAirMail,
+        Express
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var method = ShippingMethod.Express;
+            Console.WriteLine(method);
+
+            var methodID = 3;
+            Console.WriteLine((ShippingMethod)methodID);
+
+            var methodName = "Express";
+            var shippingMethod = (ShippingMethod) Enum.Parse(typeof(ShippingMethod), methodName);
+            Console.WriteLine(shippingMethod);
+        }
+    }
+}
